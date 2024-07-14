oresat/oresat-od-database

commit 63f8766959d5665f5c01e731b8f7b1bb373443a6

Move oresat mission arg handling into argparse
This commit enhances the oresat mission argument in scripts with
awareness of valid missions and the currently active mission. It now:
 - Lists valid values in the help message
 - Generates the list of valid values automatically
 - Picks an appropriate default argument (the currently active mission)
   automatically
 - Rejects invalid values in the argument parser itself

It tries to be similar but not compatible to the previous handling:
 - Accepts values of the form "oresat0.5" but also accepts values without
   the prefix, "0.5".
 - Is a flag instead of a positional argument. "--oresat 0.5" looks
   similar to the previous positional argument "oresat0.5". Since it
   picks an appropriate default value now, most uses can be elided, so
   the slightly longer option I felt was acceptable.

So far every script uses this argument, which would make it a candidate
for putting it in __main__ (the top level argument handler), but since
each script currently needs to be able to be run as a standalone as
well, I didn't see a good way of enabling that without duplicating the
option in each script as well. If it's in each script anyway then
there's not much point in having it at the top level.

To support this arg the Consts enum is introduced. It bundles short
names ("0.5"), nice names ("OreSat0.5", via __str__()) and IDs (2)
together as well as a handful of utilities for converting between
types. This grew out of OreSatId but was renamed because it no longer
is just an ID. It's a dataclass enum instead of an IntEnum, because I
don't believe the special properties of an IntEnum were being used.
IntEnum lets integer operations (arithmetic, ordered comparisons, etc.)
be used directly on the enum members but they wouldn't really make sense
for a mission ID. Maybe ordered comparisons to find out which mission
was first, but that isn't how it's been used so far. Consts can also be

oresat_configs/constants.py
@@ -4,25 +4,69 @@ OreSat OD constants
 Seperate from __init__.py to avoid cirular imports.
 """
 
-from enum import IntEnum
+from __future__ import annotations
+from dataclasses import dataclass
+from enum import Enum, IntEnum, unique
 
 __version__ = "0.3.1"
 
 
+@dataclass
+class Mission:
+    """A specific set of constants associated with an OreSat Mission"""
+    id: int
+    arg: str
+
+
+@unique
+class Consts(Mission, Enum):
+    """Constants associated with each OreSat Mission"""
+    ORESAT0 = 1, "0"
+    ORESAT0_5 = 2, "0.5"
+    ORESAT1 = 3, "1"
+
+    def __str__(self) -> str:
+        return "OreSat" + self.arg
+
+    @classmethod
+    def default(cls) -> Consts:
+        """Returns the currently active mission"""
+        return cls.ORESAT0_5
+
+    @classmethod
+    def from_string(cls, val: str) -> Consts:
+        """Fetches the Mission associated with an appropriate string
+
+        Appropriate strings are the arg (0, 0.5, ...), optionally prefixed with
+        OreSat or oresat
+        """
+        arg = val.lower().removeprefix("oresat")
+        for m in cls:
+            if m.arg == arg:
+                return m
+        raise ValueError(f"invalid oresat mission: {val}")
+
+    @classmethod
+    def from_id(cls, val: OreSatId | int) -> Consts:
+        """Fetches the Mission associated with an integer ID"""
+        if isinstance(val, OreSatId):
+            val = val.value
+        elif not isinstance(val, int):
+            raise TypeError(f"Unsupported val type: '{type(val)}'")
+
+        for m in cls:
+            if m.id == val:
+                return m
+        raise ValueError(f"invalid OreSatId: {val}")
+
+
+@unique
 class OreSatId(IntEnum):
     """Unique ID for each OreSat."""
 
     ORESAT0 = 1
     ORESAT0_5 = 2
     ORESAT1 = 3
-
-
-ORESAT_NICE_NAMES = {
-    OreSatId.ORESAT0: "OreSat0",
-    OreSatId.ORESAT0_5: "OreSat0.5",
-    OreSatId.ORESAT1: "OreSat1",
-}
-"""Nice name for OreSat missions."""
 
 
 class NodeId(IntEnum):

oresat_configs/scripts/gen_xtce.py
@@ -1,6 +1,5 @@
 """Generate XTCE for the beacon."""
 
-import sys
 import xml.etree.ElementTree as ET
 from argparse import ArgumentParser, Namespace
 from datetime import datetime
@@ -8,7 +7,7 @@ from typing import Optional
 
 import canopen
 
-from .. import ORESAT_NICE_NAMES, OreSatConfig, OreSatId
+from .. import OreSatConfig, Consts
 
 GEN_XTCE = "generate beacon xtce file"
 GEN_XTCE_PROG = "oresat-gen-xtce"
@@ -20,9 +19,9 @@ def build_parser(parser: ArgumentParser) -> ArgumentParser:
     The given parser may be standalone or it may be used as a subcommand in another ArgumentParser.
     """
     parser.description = GEN_XTCE
-    parser.add_argument(
-        "oresat", default="oresat0", help="oresat mission; oresat0, oresat0.5, or oresat1"
-    )
+    parser.add_argument("--oresat", default=Consts.default().arg, choices=[m.arg for m in Consts],
+                        type=lambda x: x.lower().removeprefix("oresat"),
+                        help="oresat mission, defaults to %(default)s")
     parser.add_argument("-d", "--dir-path", default=".", help='directory path; defautl "."')
     return parser
 
@@ -115,7 +114,7 @@ def write_xtce(config: OreSatConfig, dir_path: str = "."):
     root = ET.Element(
         "SpaceSystem",
         attrib={
-            "name": ORESAT_NICE_NAMES[config.oresat_id],
+            "name": str(config.oresat),
             "xmlns:xtce": "http://www.omg.org/space/xtce",
             "xmlns:xsi": "http://www.w3.org/2001/XMLSchema-instance",
             "xsi:schemaLocation": (
@@ -338,7 +337,7 @@ def write_xtce(config: OreSatConfig, dir_path: str = "."):
     # write
     tree = ET.ElementTree(root)
     ET.indent(tree, space="  ", level=0)
-    file_name = f"{config.oresat_id.name.lower()}.xtce"
+    file_name = f"{config.oresat.name.lower()}.xtce"
     tree.write(f"{dir_path}/{file_name}", encoding="utf-8", xml_declaration=True)
 
 
@@ -347,16 +346,5 @@ def gen_xtce(args: Optional[Namespace] = None):
     if args is None:
         args = build_parser(ArgumentParser()).parse_args()
 
-    arg_oresat = args.oresat.lower()
-    if arg_oresat in ["0", "oresat0"]:
-        oresat_id = OreSatId.ORESAT0
-    elif arg_oresat in ["0.5", "oresat0.5"]:
-        oresat_id = OreSatId.ORESAT0_5
-    elif arg_oresat in ["1", "oresat1"]:
-        oresat_id = OreSatId.ORESAT1
-    else:
-        print(f"invalid oresat mission: {args.oresat}")
-        sys.exit()
-
-    config = OreSatConfig(oresat_id)
+    config = OreSatConfig(args.oresat)
     write_xtce(config, args.dir_path)

docs/scripts/gen_beacon_rst.py
@@ -10,7 +10,7 @@ sys.path.insert(0, _FILE_PATH)
 import bitstring
 import canopen
 
-from oresat_configs import OreSatConfig, OreSatId
+from oresat_configs import OreSatConfig, Consts
 
 OD_DATA_TYPES = {
     canopen.objectdictionary.BOOLEAN: "bool",
@@ -170,8 +170,8 @@ def gen_beacon_rst(config: OreSatConfig, file_path: str, url: str):
         if obj.name in ["start_chars", "revision"]:
             desc += f": {obj.value}\n"
         if obj.name == "satellite_id":
-            sat_id = OreSatId(obj.value)
-            desc += f": {sat_id.value}\n"
+            sat = Consts.from_id(obj.value)
+            desc += f": {sat.id}\n"
         if obj.value_descriptions:
             desc += "\n\nValue Descriptions:\n"
             for value, descr in obj.value_descriptions.items():
@@ -206,7 +206,7 @@ def gen_beacon_rst_files():
     """Generate all beacon rst files."""
 
     parent_dir = os.path.dirname(os.path.abspath(__file__ + "/.."))
-    for mission in list(OreSatId):
+    for mission in Consts:
         mission_name = mission.name.lower()
         url = (
             f"https://github.com/oresat/oresat-configs/blob/master/oresat_configs/{mission_name}"

oresat_configs/__init__.py
@@ -15,7 +15,7 @@ from ._yaml_to_od import (
 )
 from .base import FW_COMMON_CONFIG_PATH
 from .beacon_config import BeaconConfig
-from .constants import ORESAT_NICE_NAMES, NodeId, OreSatId, __version__
+from .constants import Consts, NodeId, OreSatId, __version__
 from .oresat0 import ORESAT0_BEACON_CONFIG_PATH, ORESAT0_CARD_CONFIGS_PATH
 from .oresat0_5 import ORESAT0_5_BEACON_CONFIG_PATH, ORESAT0_5_CARD_CONFIGS_PATH
 from .oresat1 import ORESAT1_BEACON_CONFIG_PATH, ORESAT1_CARD_CONFIGS_PATH
@@ -55,11 +55,17 @@ class OreSatConfig:
         OreSatId.ORESAT1: ORESAT1_BEACON_CONFIG_PATH,
     }
 
-    def __init__(self, oresat_id: OreSatId):
-        self.oresat_id = oresat_id
-        beacon_config_path = self.BEACON_CONFIG_PATHS[oresat_id]
+    def __init__(self, oresat: OreSatId | Consts | str):
+        if isinstance(oresat, str):
+            oresat = Consts.from_string(oresat)
+        elif isinstance(oresat, OreSatId):
+            oresat = Consts.from_id(oresat)
+        elif not isinstance(oresat, Consts):
+            raise TypeError(f"Unsupported oresat type: '{type(oresat)}'")
+        self.oresat = oresat
+        beacon_config_path = self.BEACON_CONFIG_PATHS[oresat.id]
         beacon_config = BeaconConfig.from_yaml(beacon_config_path)
-        card_configs_path = self.CARD_CONFIG_PATHS[oresat_id]
+        card_configs_path = self.CARD_CONFIG_PATHS[oresat.id]
 
         self.cards = {}
         file_path = f"{os.path.dirname(os.path.abspath(__file__))}/cards.csv"
@@ -79,8 +85,8 @@ class OreSatConfig:
                     )
 
         self.configs = _load_configs(card_configs_path)
-        self.od_db = _gen_od_db(oresat_id, self.cards, beacon_config, self.configs)
+        self.od_db = _gen_od_db(oresat, self.cards, beacon_config, self.configs)
         c3_od = self.od_db["c3"]
         self.beacon_def = _gen_c3_beacon_defs(c3_od, beacon_config)
         self.fram_def = _gen_c3_fram_defs(c3_od, self.configs["c3"])
-        self.fw_base_od = _gen_fw_base_od(oresat_id, FW_COMMON_CONFIG_PATH)
+        self.fw_base_od = _gen_fw_base_od(oresat, FW_COMMON_CONFIG_PATH)

oresat_configs/_yaml_to_od.py
@@ -14,7 +14,7 @@ except ImportError:
 
 from .beacon_config import BeaconConfig
 from .card_config import CardConfig, IndexObject
-from .constants import OreSatId, __version__
+from .constants import Consts, __version__
 
 STD_OBJS_FILE_NAME = f"{os.path.dirname(os.path.abspath(__file__))}/standard_objects.yaml"
 
@@ -564,7 +564,7 @@ def _load_configs(config_paths: dict) -> Dict[str, CardConfig]:
     return configs
 
 
-def _gen_od_db(oresat_id: OreSatId, cards: dict, beacon_def: BeaconConfig, configs: dict) -> dict:
+def _gen_od_db(oresat: Consts, cards: dict, beacon_def: BeaconConfig, configs: dict) -> dict:
     od_db = {}
     node_ids = {name: cards[name].node_id for name in configs}
     node_ids["c3"] = 0x1
@@ -606,9 +606,9 @@ def _gen_od_db(oresat_id: OreSatId, cards: dict, beacon_def: BeaconConfig, confi
 
         # set specific obj defaults
         od["versions"]["configs_version"].default = __version__
-        od["satellite_id"].default = oresat_id.value
-        for oid in list(OreSatId):
-            od["satellite_id"].value_descriptions[oid.value] = oid.name.lower()
+        od["satellite_id"].default = oresat.id
+        for sat in Consts:
+            od["satellite_id"].value_descriptions[sat.id] = sat.name.lower()
         if name == "c3":
             od["beacon"]["revision"].default = beacon_def.revision
             od["beacon"]["dest_callsign"].default = beacon_def.ax25.dest_callsign
@@ -672,7 +672,7 @@ def _gen_c3_beacon_defs(c3_od: canopen.ObjectDictionary, beacon_def: BeaconConfi
     return beacon_objs
 
 
-def _gen_fw_base_od(oresat_id: OreSatId, config_path: str) -> canopen.ObjectDictionary:
+def _gen_fw_base_od(oresat: Consts, config_path: str) -> canopen.ObjectDictionary:
     """Generate all ODs for a OreSat mission."""
 
     od = canopen.ObjectDictionary()
@@ -709,6 +709,6 @@ def _gen_fw_base_od(oresat_id: OreSatId, config_path: str) -> canopen.ObjectDict
 
     # set specific obj defaults
     od["versions"]["configs_version"].default = __version__
-    od["satellite_id"].default = oresat_id.value
+    od["satellite_id"].default = oresat.id
 
     return od

oresat_configs/scripts/gen_dcf.py
@@ -1,13 +1,12 @@
 """Generate a DCF for from an OreSat card's object directory."""
 
-import sys
 from argparse import ArgumentParser, Namespace
 from datetime import datetime
 from typing import Optional
 
 import canopen
 
-from .. import OreSatConfig, OreSatId
+from .. import OreSatConfig, Consts
 
 GEN_DCF = "generate DCF file for OreSat node(s)"
 GEN_DCF_PROG = "oresat-gen-dcf"
@@ -19,9 +18,9 @@ def build_parser(parser: ArgumentParser) -> ArgumentParser:
     The given parser may be standalone or it may be used as a subcommand in another ArgumentParser.
     """
     parser.description = GEN_DCF
-    parser.add_argument(
-        "oresat", default="oresat0", help="oresat mission; oresat0, oresat0.5, or oresat1"
-    )
+    parser.add_argument("--oresat", default=Consts.default().arg, choices=[m.arg for m in Consts],
+                        type=lambda x: x.lower().removeprefix("oresat"),
+                        help="oresat mission, defaults to %(default)s")
     parser.add_argument("card", help="card name; all, c3, gps, star_tracker_1, etc")
     parser.add_argument("-d", "--dir-path", default=".", help='directory path; defautl "."')
     return parser
@@ -242,18 +241,7 @@ def gen_dcf(args: Optional[Namespace] = None):
     if args is None:
         args = build_parser(ArgumentParser()).parse_args()
 
-    arg_oresat = args.oresat.lower()
-    if arg_oresat in ["0", "oresat0"]:
-        oresat_id = OreSatId.ORESAT0
-    elif arg_oresat in ["0.5", "oresat0.5"]:
-        oresat_id = OreSatId.ORESAT0_5
-    elif arg_oresat in ["1", "oresat1"]:
-        oresat_id = OreSatId.ORESAT1
-    else:
-        print(f"invalid oresat mission: {args.oresat}")
-        sys.exit()
-
-    config = OreSatConfig(oresat_id)
+    config = OreSatConfig(args.oresat)
 
     if args.card.lower() == "all":
         for od in config.od_db.values():

oresat_configs/scripts/gen_fw_files.py
@@ -8,7 +8,7 @@ from typing import Optional
 
 import canopen
 
-from .. import OreSatConfig, OreSatId
+from .. import OreSatConfig, Consts
 
 GEN_FW_FILES = "generate CANopenNode OD.[c/h] files for a OreSat firmware card"
 GEN_FW_FILES_PROG = "oresat-gen-fw-files"
@@ -20,7 +20,9 @@ def build_parser(parser: ArgumentParser) -> ArgumentParser:
     The given parser may be standalone or it may be used as a subcommand in another ArgumentParser.
     """
     parser.description = GEN_FW_FILES
-    parser.add_argument("oresat", help="oresat mission; oresat0 or oresat0.5")
+    parser.add_argument("--oresat", default=Consts.default().arg, choices=[m.arg for m in Consts],
+                        type=lambda x: x.lower().removeprefix("oresat"),
+                        help="oresat mission, defaults to %(default)s")
     parser.add_argument("card", help="card name; c3, battery, solar, imu, or reaction_wheel")
     parser.add_argument("-d", "--dir-path", default=".", help='output directory path, default: "."')
     return parser
@@ -678,18 +680,7 @@ def gen_fw_files(args: Optional[Namespace] = None):
     if args is None:
         args = build_parser(ArgumentParser()).parse_args()
 
-    arg_oresat = args.oresat.lower()
-    if arg_oresat in ["0", "oresat0"]:
-        oresat_id = OreSatId.ORESAT0
-    elif arg_oresat in ["0.5", "oresat0.5"]:
-        oresat_id = OreSatId.ORESAT0_5
-    elif arg_oresat in ["1", "oresat1"]:
-        oresat_id = OreSatId.ORESAT1
-    else:
-        print(f"invalid oresat mission: {args.oresat}")
-        sys.exit()
-
-    config = OreSatConfig(oresat_id)
+    config = OreSatConfig(args.oresat)
 
     arg_card = args.card.lower().replace("-", "_")
     if arg_card == "c3":

oresat_configs/scripts/print_od.py
@@ -1,12 +1,11 @@
 """Print out a card's objects directory."""
 
-import sys
 from argparse import ArgumentParser, Namespace
 from typing import Any, Optional
 
 import canopen
 
-from .. import OreSatConfig, OreSatId
+from .. import OreSatConfig, Consts
 from .._yaml_to_od import OD_DATA_TYPES
 
 PRINT_OD = "print the object dictionary out to stdout"
@@ -19,7 +18,9 @@ def build_parser(parser: ArgumentParser) -> ArgumentParser:
     The given parser may be standalone or it may be used as a subcommand in another ArgumentParser.
     """
     parser.description = PRINT_OD
-    parser.add_argument("oresat", default="oresat0", help="oresat mission; oresat0 or oresat0.5")
+    parser.add_argument("--oresat", default=Consts.default().arg, choices=[m.arg for m in Consts],
+                        type=lambda x: x.lower().removeprefix("oresat"),
+                        help="oresat mission, defaults to %(default)s")
     parser.add_argument("card", help="card name; c3, gps, star_tracker_1, etc")
     return parser
 
@@ -52,18 +53,7 @@ def print_od(args: Optional[Namespace] = None):
     if args is None:
         args = build_parser(ArgumentParser()).parse_args()
 
-    arg_oresat = args.oresat.lower()
-    if arg_oresat in ["0", "oresat0"]:
-        oresat_id = OreSatId.ORESAT0
-    elif arg_oresat in ["0.5", "oresat0.5"]:
-        oresat_id = OreSatId.ORESAT0_5
-    elif arg_oresat in ["1", "oresat1"]:
-        oresat_id = OreSatId.ORESAT1
-    else:
-        print(f"invalid oresat mission: {args.oresat}")
-        sys.exit()
-
-    config = OreSatConfig(oresat_id)
+    config = OreSatConfig(args.oresat)
 
     inverted_od_data_types = {}
     for key, value in OD_DATA_TYPES.items():

commit b5a591ed104cf7faff6acdf1cc2866fbf773304f

Validate XTCE output
Adds a test that will validate the generated oresat xtce against the
official xtce schema. This caught three minor issues in our generator:
- The root `name` attribute doesn't like dots in its string. 63f87669
  broke this when refactoring the mission names, this restores the old
  behavior.
- The XML namespace was incorrect. It pointed to the old version of XTCE
  which had been changed in the new schema. It also required `xtce` to
  be the prefix for every tag, which we weren't doing. Dropping it means
  the whole document is assumed to be in that namespace so we don't have
  to go through the hassle of prefixing every tag.
- Encodings -> Encoding.

The schema check in CI could be more elegant, I've just hardcoded both
fetching the schema and the output file for now but they probably should
be determined dynamically eventually.

.github/workflows/tests.yaml
@@ -25,7 +25,8 @@ jobs:
 
     - name: Install dependencies
       run: |
-        sudo apt install libyaml-0-2
+        sudo apt update
+        sudo apt install -y libyaml-0-2 libxml2-utils
         python -m pip install --upgrade pip
         pip install -r requirements.txt
 
@@ -55,6 +56,8 @@ jobs:
         oresat-gen-dcf c3
         oresat-print-od c3 > /dev/null
         oresat-gen-xtce
+        curl -O https://www.omg.org/spec/XTCE/20180204/SpaceSystem.xsd
+        xmllint --noout --schema SpaceSystem.xsd oresat0_5.xtce
         oresat-configs cards > /dev/null
         oresat-configs pdo c3 --list > /dev/null
         pip uninstall -y oresat-configs

oresat_configs/constants.py
@@ -47,6 +47,13 @@ class Consts(Mission, Enum):
     def __str__(self) -> str:
         return "OreSat" + self.arg
 
+    def filename(self) -> str:
+        """Returns a string safe to use in filenames and other restricted settings.
+
+        All lower case, dots replaced with underscores.
+        """
+        return str(self).lower().replace(".", "_")
+
     @classmethod
     def default(cls) -> Consts:
         """Returns the currently active mission"""

oresat_configs/scripts/gen_xtce.py
@@ -117,8 +117,8 @@ def write_xtce(config: OreSatConfig, dir_path: str = ".") -> None:
     root = ET.Element(
         "SpaceSystem",
         attrib={
-            "name": str(config.mission),
-            "xmlns:xtce": "http://www.omg.org/space/xtce",
+            "name": config.mission.filename(),
+            "xmlns": "http://www.omg.org/spec/XTCE/20180204",
             "xmlns:xsi": "http://www.w3.org/2001/XMLSchema-instance",
             "xsi:schemaLocation": (
                 "http://www.omg.org/spec/XTCE/20180204 "
@@ -175,7 +175,7 @@ def write_xtce(config: OreSatConfig, dir_path: str = ".") -> None:
             "shortDescription": "Unix coarse timestamp",
         },
     )
-    enc = ET.SubElement(para_type, "Encodings")
+    enc = ET.SubElement(para_type, "Encoding")
     ET.SubElement(
         enc,
         "IntegerDataEncoding",
@@ -340,7 +340,7 @@ def write_xtce(config: OreSatConfig, dir_path: str = ".") -> None:
     # write
     tree = ET.ElementTree(root)
     ET.indent(tree, space="  ", level=0)
-    file_name = f"{config.mission.name.lower()}.xtce"
+    file_name = f"{config.mission.filename()}.xtce"
     tree.write(f"{dir_path}/{file_name}", encoding="utf-8", xml_declaration=True)
 
 